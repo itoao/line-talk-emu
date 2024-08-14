6feb4b84c1d471386b0f70bb91a89d0a97d90477
aoito <ao.ito@tabian.co>
2024-08-15 02:02:05 +0900

    📝 title

--- src/components.tsx
+++ src/components.tsx
@@ -11,11 +11,13 @@ export const renderer = jsxRenderer(({ children }) => {
         <script src="https://unpkg.com/htmx.org@1.9.3"></script>
         <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
         <script src="https://cdn.tailwindcss.com"></script>
-        <title>Hono + htmx</title>
+        <title>LINEトーク履歴エミュレーター</title>
       </head>
       <body>
         <div class="p-4">
-          <h1 class="text-4xl font-bold mb-4"><a href="/">Todo</a></h1>
+          <div class="bg-[#06c755] p-3">
+            <h4 class="font-bold mb-4 text-white"><a href="/">LINEトーク履歴エミュレーター</a></h1>
+          </div>
           ${children}
         </div>
       </body>
